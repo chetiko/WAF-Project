5dde1346f62f36754f78fba22a5ff3897a7d7981
Marsella Pérez <marse.mp24@gmail.com>
2024-08-20 22:21:27 -0600

    Update whitelisted_ips.php

--- data/whitelisted_ips.php
+++ data/whitelisted_ips.php
@@ -1,1 +1,8 @@
+<?php
+return [
+    '192.168.1.1',       // IP específica
+    '203.0.113.0/24',    // Rango de IP (CIDR)
+    '198.51.100.14',     // Otra IP específica
+    // Agrega más IPs o rangos de IP aquí
+];
 
